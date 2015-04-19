4338b0dcada5f895402746b5c07fc587f458ffaf
cdcolen <cdcolen@att.net>
2015-04-19 04:28:04 -0400

    joj

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,2 +1,6 @@
 Session.set('hasAccount', true);
 Session.set('universityNotPresent', false);
+Session.set('homeActive',true);
+Session.set('aboutUsActive',false);
+Session.set('profileActive',false);
+Session.set('courseSearchActive',false);
