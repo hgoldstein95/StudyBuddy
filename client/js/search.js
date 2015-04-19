14ec7df962041cc2ac4ee45b0146691243022dc8
cdcolen <cdcolen@att.net>
2015-04-19 11:24:20 -0400

    massive errors

--- client/js/search.js
+++ client/js/search.js
@@ -1,6 +1,10 @@
 Template.search.events({
 	'submit form': function(evt) {
 		evt.preventDefault();
-		$('.search-results').prepend("Yolo");
+		var college=UserInfo.university;
+		if(Universities.find({"name":college}).fetch[0]!=undefined){
+			var index=Universities.find({"name":college}).fetch[0].classes.indexOf(evt.target.searchForClasses.value);
+			$('.search-results').prepend(Universities.find({"name":college}).fetch[0].classes.indexOf(evt.target.searchForClasses.value)[index]);
+		}
 	}
 })
