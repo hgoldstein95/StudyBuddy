063ee6b233da5f54091dff83dd6104f299f91b0c
Harrison Goldstein <hgoldstein95@gmail.com>
2015-04-21 23:12:19 -0400

    Added config for accounts.

---
+++ server/config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "857338500687-4hqn46ncjfjnv0ma3280qmg04lj5525g.apps.googleusercontent.com",
+  secret: "UXNCPKgl9LjOZdjbf2NbHybT"
+});
